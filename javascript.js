f3eb4cdee9a9f3867f4d72d33654e553afcf0bd9
houssemdev1 <houssem.najeh@outlook.com>
2018-10-31 16:06:05 +0100

    Add user

--- javascript.js
+++ javascript.js
@@ -67,8 +67,8 @@ function Save() {
   };
 
 
-  // Retrieve the object from storage
-  var retrievedTab = JSON.parse(localStorage.getItem("tableau"));
+  // Retrieve the object from stora  ge
+var retrievedTab = JSON.parse(localStorage.getItem("tableau"));
   if (retrievedTab == null) {
 
 if((document.getElementById("inputLogin").value == "")||(document.getElementById("inputPrenom").value == "")||(document.getElementById("inputPassword").value == "")||(document.getElementById("inputEmail").value == "")||( document.getElementById("inputAdresse").value == "")||(document.getElementById("inputCin").value == "")||(document.getElementById("inputTel").value == "")||(document.getElementById("inputSociete").value =="")){
@@ -162,3 +162,154 @@ function sendemail()
                   var body = ("..");
                    location.href="mailto:" + email + '?subject=' +subject+ '&body=' +body+ "" ;
               }
+
+
+/**fonction d'ajout utilisateur */
+function add_user()
+
+{
+  /**inputs de formulaire ajout employe */
+  var id_user = document.getElementById("inputid").value;
+  var image=document.getElementById("img").value;
+  var nom_user = document.getElementById("inputnom_user").value;
+  var Prenom_user = document.getElementById("inputprenom_user").value;
+  var Email_user = document.getElementById("inputEmail_user").value;
+  var Pwd_user = document.getElementById("inputpwd_user").value;
+  var CIN_user = document.getElementById("inputCin_user").value;
+  var date_nais = document.getElementById("inputdatenais").value;
+  var Adresse_user = document.getElementById("inputadresse_user").value;
+  var Tel_user = document.getElementById("inputtel_user").value;
+  var Societe = document.getElementById("inputSociete_user").value;
+  var salaire = document.getElementById("inpusalaire").value;
+  var congé = document.getElementById("inputconge").value;
+  
+  var employe = {
+    'id': id_user,
+    'img':image,
+    'lastname': nom_user,
+    'firstname': Prenom_user,
+    'datenais':date_nais,
+    'pwd': Pwd_user,
+    'email': Email_user,
+    'CIN': CIN_user,
+    'adresse': Adresse_user,
+    'tel': Tel_user,
+    'societe': Societe,
+'salaire':salaire,
+'congé':congé
+
+  };
+  /**tableau des employes */
+  var userTab = [];
+
+  
+  if(localStorage.length>0)
+ {
+   userTab=JSON.parse(localStorage.getItem("tabemploye"));
+  userTab.push(employe); 
+  localStorage.setItem("tabemploye",JSON.stringify(userTab));
+ }
+ else{
+  userTab.push(employe); 
+  localStorage.setItem("tabemploye",JSON.stringify(userTab));
+
+}
+alert("c bon");
+
+}
+
+
+
+
+/**fonction affichage de profil employe */
+
+function user_profil()
+{
+  
+  var i;
+  var profil = "<tr class=\"first\"></tr><tr><b>Email/ID</b><br></tr><tr><b>Prenom</b><br></tr><tr><b>Nom</b><br></tr><tr><b>Date De Naissance</b><br></tr></tr> \n";
+  document.getElementById("profil").innerHTML = profil;
+  /**valeur de recupertion de de l'email de l'utilisateur connecté */
+  var l=JSON.parse(localStorage.getItem("objlogin"));
+  var res=JSON.parse(localStorage.getItem("tabemploye"));
+  
+  for(i=0;i<res.length;i++)
+  {
+   
+    if(res[i].email==l)
+  {
+  
+   profil = "<tr class=\"first\"></tr><tr>"+res[i].img+"</tr><tr><td><b>Email/ID:</b></td><td>"+res[i].id+"</td></tr><tr><td><b>Nom:</b></td><td>"+res[i].lastname+"</td></tr><tr><td><b>Prenom:</b></td><td>"+res[i].firstname+"</td></tr><tr><td><b>Date de naissance:</b></td><td>"+res[i].datenais+"</td></tr><tr><td><b>Mot de passe:</b></td><td>"+res[i].pwd+"</td></tr><tr><td><b>CIN:</b></td><td>"+res[i].CIN+"</td></tr><tr><td><b>Adresse:</b></td><td>"+res[i].adresse+"</td></tr><tr><td><b>Tel:</b></td><td>"+res[i].tel+"</td></tr><tr><td><b>Societe:</b></td><td>"+res[i].societe+"</td></tr><tr><td><b>Salaire:</b></td><td>"+res[i].salaire+"</td></tr><tr><td><b>Congé:</b></td><td>"+res[i].congé+"</td></tr> \n";
+ 
+   document.getElementById("profil").innerHTML = profil;
+}
+}
+}
+/***fonction authentifier */
+function authentifier_user() {
+    var i;
+    /*variable de recuperation de l'adresse email user*/
+ var vlogin=document.getElementById("login");
+ /*variable de recuperation du tableau de localstorage*/
+ var  val=JSON.parse(localStorage.getItem("tabemploye"));
+    for(i=0;i<val.length;i++)
+  {
+    if(val[i].email==vlogin)
+    {
+        se_connecter();
+        return 1;
+    }
+     else 
+     { 
+         alert("utilisateur  introuvable");
+     }
+
+
+
+  }
+}
+/*fonction se connecter*/
+function se_connecter()
+{
+  /**variable de recuperation adrsse email dans localstorage */
+  
+var log=document.getElementById("login").value;
+var retrievedTabUser = JSON.parse(localStorage.getItem("tabemploye"));
+
+localStorage.setItem("objlogin",JSON.stringify(log));
+
+    location.href="InterfaceEmploye/g_consulter_profil.html";
+    user_profil();
+    
+    
+}
+/*tester la validation de l'email*/
+function validation()
+         {
+            var expressionReguliere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+
+            if (!(expressionReguliere.test(document.getElementById("email").value)))
+          {  document.getElementById("pemail").innerHTML="adresse email non valide!";
+
+        }
+        else if(document.getElementById("password").value==""){
+            document.getElementById("ppassword").innerHTML="verifiez votre mot de passe!";
+
+         }
+          else{
+       authentifier_user();
+      
+        }
+        
+    /* document.getElementById("i").addEventListener("click",function(eve){eve.preventDefault()})  ; */  
+         }
+        
+        /**fonction de deconnexion */
+        function deconnecter()
+        {
+localStorage.removeItem("objlogin");
+localStorage.removeItem("key");
+
+location.href="../login.html";
+
+        }
