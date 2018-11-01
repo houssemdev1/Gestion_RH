0922940086bf817c9c77679c7407ef8e25832653
houssemdev1 <houssem.najeh@outlook.com>
2018-11-01 12:04:42 +0100

    Change at Tuesday

--- javascript.js
+++ javascript.js
@@ -33,7 +33,7 @@ window.onclick = function (event) {
 
 
 /*** Storage******/
-id = 0;
+
 function Save() {
   //var id = document.getElementById("inputId").value;
   var Nom = document.getElementById("inputLogin").value;
@@ -67,25 +67,23 @@ function Save() {
   };
 
 
-
-  // Retrieve the object from storage
-  var retrievedTab = JSON.parse(localStorage.getItem("tableauAdmin"));
-
+  // Retrieve the object from stora  ge
+var retrievedTab = JSON.parse(localStorage.getItem("tableau"));
   if (retrievedTab == null) {
 
-    if ((document.getElementById("inputLogin").value == "") || (document.getElementById("inputPrenom").value == "") || (document.getElementById("inputPassword").value == "") || (document.getElementById("inputEmail").value == "") || (document.getElementById("inputAdresse").value == "") || (document.getElementById("inputCin").value == "") || (document.getElementById("inputTel").value == "") || (document.getElementById("inputSociete").value == "")) {
+if((document.getElementById("inputLogin").value == "")||(document.getElementById("inputPrenom").value == "")||(document.getElementById("inputPassword").value == "")||(document.getElementById("inputEmail").value == "")||( document.getElementById("inputAdresse").value == "")||(document.getElementById("inputCin").value == "")||(document.getElementById("inputTel").value == "")||(document.getElementById("inputSociete").value =="")){
 
-      myFunctionSnackbarEchoué()
-    }
-    else {
-      retrievedTab = [];
-      id == id++;
+  myFunctionSnackbarEchoué()
+}
+    else{
+    retrievedTab = [];
+    id == id++;
 
-      retrievedTab.push(person);
-      localStorage.setItem("tableauAdmin", JSON.stringify(retrievedTab));
+    retrievedTab.push(person);
+    localStorage.setItem("tableau", JSON.stringify(retrievedTab));
 
-      console.log(retrievedTab);
-      myFunctionSnackbarReussi()
+    console.log(retrievedTab);
+    myFunctionSnackbarReussi() 
     }
   }
   if (retrievedTab != null) {
@@ -93,9 +91,9 @@ function Save() {
 
     for (i = 0; i < retrievedTab.length; i++) {
       //Contrôle InputText 
-      if ((document.getElementById("inputLogin").value == "") || (document.getElementById("inputPrenom").value == "") || (document.getElementById("inputPassword").value == "") || (document.getElementById("inputEmail").value == "") || (document.getElementById("inputAdresse").value == "") || (document.getElementById("inputCin").value == "") || (document.getElementById("inputTel").value == "") || (document.getElementById("inputSociete").value == "") || (document.getElementById("inputPrenom").value == retrievedTab[i].firstName) || (document.getElementById("inputEmail").value == retrievedTab[i].email) || (document.getElementById("inputCin").value == retrievedTab[i].cin) || (document.getElementById("inputSociete").value == retrievedTab[i].societe)) {
+      if ( (document.getElementById("inputLogin").value == "")||(document.getElementById("inputPrenom").value == "")||(document.getElementById("inputPassword").value == "")||(document.getElementById("inputEmail").value == "")||( document.getElementById("inputAdresse").value == "")||(document.getElementById("inputCin").value == "")||(document.getElementById("inputTel").value == "")||(document.getElementById("inputSociete").value =="")||(document.getElementById("inputPrenom").value == retrievedTab[i].firstName) || (document.getElementById("inputEmail").value == retrievedTab[i].email) || (document.getElementById("inputCin").value == retrievedTab[i].cin) || (document.getElementById("inputSociete").value == retrievedTab[i].societe)) {
         resultat = 0;
-
+        
 
 
 
@@ -112,10 +110,8 @@ function Save() {
       id == id++;
 
       retrievedTab.push(person);
-      localStorage.setItem("tableauAdmin", JSON.stringify(retrievedTab));
-      myFunctionSnackbarReussi();
-      var key = retrievedTab[i].societe;
-      localStorage.setItem("key", key)
+      localStorage.setItem("tableau", JSON.stringify(retrievedTab));
+      myFunctionSnackbarReussi() 
     }
     if (a == 0) {
       console.log('user mawjoud');
@@ -128,7 +124,6 @@ function Save() {
 
 
 /*****Fonction Sncakbar ******/
-
 // Fonction Snackbar Création Réussie
 function myFunctionSnackbarReussi() {
   var x = document.getElementById("snackbar");
@@ -137,104 +132,55 @@ function myFunctionSnackbarReussi() {
 }
 
 //Fonction Snackbar Création échoué
-function myFunctionSnackbarEchoué() {
+function myFunctionSnackbarEchoué(){
   var x = document.getElementById("snackbarFailed");
   x.className = "show";
-  setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+  setTimeout(function(){ x.className = x.className.replace("show","");},3000);
 
 }
 
 /**fonction de recuperation de passwd */
-function recuperer_passwd() {
-  var expressionReguliere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+function recuperer_passwd()
+{
+    var expressionReguliere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
 
-  if (!(expressionReguliere.test(document.getElementById("email").value))) {
-    document.getElementById("pemail").innerHTML = "adresse email non valide!";
-  }
-  else {
-    sendemail();
-  }
+    if (!(expressionReguliere.test(document.getElementById("email").value)))
+  {  
+      document.getElementById("pemail").innerHTML="adresse email non valide!";
+}
+  else
+  {
+sendemail();
+}
 
 }
 /**fonction d'envoi d'email */
-function sendemail() {
-  var email = document.getElementById("email").value;
-  var subject = ("nouveau mot de passe");
-  var body = ("..");
-  location.href = "mailto:" + email + '?subject=' + subject + '&body=' + body + "";
-}
-
-
-
-
-/******Se Connecter() As User and Admin*******/
-var resultatAdmin ;
-var resultatUser ;
-function Login() {
-  var login = document.getElementById("inputLogin").value;
-  var pwd = document.getElementById("inputPassword").value;
-
-  //Retrieve from local storage table
-  var retrievedTab = JSON.parse(localStorage.getItem("tableauAdmin")) || [];
-  var retrievedTabUser = JSON.parse(localStorage.getItem("tableauUser")) || [];
-
-
-
-  //Control Table User and Admin
-  if ((retrievedTab == null)) {
-
-  }
-
-  if (retrievedTab != null) {
-    if ((document.getElementById("inputLogin").value === "") || (document.getElementById("inputPassword").value === "")) 
-    {
-
-      console.log("input missed");
-
-    }
-    if ((document.getElementById("inputLogin").value !== "") && (document.getElementById("inputPassword").value !== "")) 
-    {
-      key = false;
-
-      for (i = 0; i < retrievedTab.length; i++)
-       {
-
-        if ((document.getElementById("inputLogin").value == retrievedTab[i].email) && (document.getElementById("inputPassword").value == retrievedTab[i].pwd)) 
-        {
-          resultatAdmin = 1;
-          resultatUser = 0 ;
-          key = retrievedTab[i].societe;
-          
-
-        }
-        
-   
-        if ((document.getElementById("inputLogin").value != retrievedTab[i].email) && (document.getElementById("inputPassword").value !== retrievedTab[i].pwd)) 
-        {
-          
-          if (retrievedTabUser == null) {
-
-          }
-          
-
-          if (retrievedTabUser !== null) 
-          {
-            for (j = 0; j < retrievedTabUser.length; j++) {
-              
-              if (((document.getElementById("inputLogin").value == retrievedTabUser[j].email) && (document.getElementById("inputPassword").value == retrievedTabUser[j].pwd))) 
+function sendemail()
               {
-                resultatAdmin = 0 ;
-                resultatUser = 1 ;
-                key = retrievedTabUser[j].societe;
-              }
-<<<<<<< HEAD
+                  var email = document.getElementById("email").value;
+                  var subject = ("nouveau mot de passe");
+                  var v_email=document.getElementById("email").value;
+             var     userTab=JSON.parse(localStorage.getItem("tabemploye"));    
+  
+  for(i=0;i<userTab.length;i++)
+  {
+   
+    if(userTab[i].email==v_email)
+  {
+  
+                  var body = (userTab[i].pwd);
+                   location.href="mailto:" + v_email + '?subject=' +subject+ '&body=' +body+ "" ;
+          }        }}
 
 
 /**fonction d'ajout utilisateur */
-function add_user()
-
-{
+/**fonction d'ajout utilisateur */
+function add_user(){
+   
   /**inputs de formulaire ajout employe */
+  /**variable pour extraire l'objet key (admin) de localstorage */
+/*  var v_key=JSON.parse(localStorage.getItem("key"));*/
+  
   var id_user = document.getElementById("inputid").value;
   var image=document.getElementById("img").value;
   var nom_user = document.getElementById("inputnom_user").value;
@@ -245,7 +191,7 @@ function add_user()
   var date_nais = document.getElementById("inputdatenais").value;
   var Adresse_user = document.getElementById("inputadresse_user").value;
   var Tel_user = document.getElementById("inputtel_user").value;
-  var Societe = document.getElementById("inputSociete_user").value;
+ /* var Societe=v_key.societe;*/
   var salaire = document.getElementById("inpusalaire").value;
   var congé = document.getElementById("inputconge").value;
   
@@ -260,7 +206,7 @@ function add_user()
     'CIN': CIN_user,
     'adresse': Adresse_user,
     'tel': Tel_user,
-    'societe': Societe,
+    /*'societe': Societe,*/
 'salaire':salaire,
 'congé':congé
 
@@ -269,7 +215,7 @@ function add_user()
   var userTab = [];
 
   
-  if(localStorage.length>0)
+  if(localStorage.length>0)//test sur les objets de localstorage 
  {
    userTab=JSON.parse(localStorage.getItem("tabemploye"));
   userTab.push(employe); 
@@ -283,7 +229,6 @@ function add_user()
 alert("c bon");
 
 }
-
 
 
 
@@ -311,120 +256,34 @@ function user_profil()
 }
 }
 }
-/***fonction authentifier */
-function authentifier_user() {
-    var i;
-    /*variable de recuperation de l'adresse email user*/
- var vlogin=document.getElementById("login");
- /*variable de recuperation du tableau de localstorage*/
- var  val=JSON.parse(localStorage.getItem("tabemploye"));
-    for(i=0;i<val.length;i++)
-  {
-    if(val[i].email==vlogin)
-    {
-        se_connecter();
-        return 1;
-    }
-     else 
-     { 
-         alert("utilisateur  introuvable");
-     }
 
 
-
-  }
-}
-/*fonction se connecter*/
-function se_connecter()
-{
-  /**variable de recuperation adrsse email dans localstorage */
-  
-var log=document.getElementById("login").value;
-var retrievedTabUser = JSON.parse(localStorage.getItem("tabemploye"));
-
-localStorage.setItem("objlogin",JSON.stringify(log));
-
-    location.href="InterfaceEmploye/g_consulter_profil.html";
-    user_profil();
-    
-    
-}
 /*tester la validation de l'email*/
 function validation()
          {
             var expressionReguliere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
 
-            if (!(expressionReguliere.test(document.getElementById("email").value)))
+            if (!(expressionReguliere.test(document.getElementById("inputLogin").value)))
           {  document.getElementById("pemail").innerHTML="adresse email non valide!";
 
         }
-        else if(document.getElementById("password").value==""){
+        else if(document.getElementById("inputPassword").value==""){
             document.getElementById("ppassword").innerHTML="verifiez votre mot de passe!";
 
          }
           else{
-       authentifier_user();
       
+      sendemail();
         }
         
     /* document.getElementById("i").addEventListener("click",function(eve){eve.preventDefault()})  ; */  
-         }
+      }
         
         /**fonction de deconnexion */
         function deconnecter()
         {
-localStorage.removeItem("objlogin");
 localStorage.removeItem("key");
 
 location.href="../login.html";
 
         }
-=======
-              if (((document.getElementById("inputLogin").value !== retrievedTabUser[j].email) && (document.getElementById("inputPassword").value !== retrievedTabUser[j].pwd))) 
-              {
-                resultatAdmin = 0;
-                resultatUser = 0 ;
-              }
-            }
-
-          }
-
-
-        }
-      }
-      console.log('resultatAdmin ', resultatAdmin);
-      console.log('resultatUser ', resultatUser);
-
-    }
-  }
-  
-
-
-
-  if (resultatAdmin == 1) {
-    location.href = 'InterfaceAdmin/Admin.html';
-    //Store
-    localStorage.setItem("Key", key);
-  }
-  if (resultatUser == 1) {
-    location.href = 'g_consulter_profil.html.html';
-    localStorage.setItem("Key", key);
-    localStorage.setItem("Key", key);
-  }
-  if ((resultatAdmin == 0) && (resultatUser == 0)) {
-    // myFunctionSnackbarEchoué()
-  }
-
-}
-
-<<<<<<< HEAD
-
-
-
-/******Afficher Users *******/
-function Afficher(){
-  
-}
-=======
->>>>>>> d6c0e3dcbbc404fbb85788311eacf3bec2c9be0e
->>>>>>> d11b6fc2736e653ca8dc5c8fe53041abe1bea89c
